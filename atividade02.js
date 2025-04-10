661f2754301df560214260c2bdc890fe36a818db
ariane1504 <ariane.o@estudante.ifro.edu.br>
2025-04-10 11:21:52 -0400

    atividade02

---
+++ atividade02.js
@@ -0,0 +1,22 @@
+let nome = `ariane`;
+let idade = 15;
+
+console.log(`ola, meu nome é`+ nome +'e eu tenho'+ idade +'anos')
+
+//2
+let string = `dois patinhos`
+palavras = string.toLowerCase();
+console.log(palavras);
+
+palavras = string.toLocaleUpperCase();
+console.log(palavras);
+
+//3
+let numero = 80;
+console.log(typeof numero)
+
+//4
+let altura = 1.50
+let peso = 23
+let soma =(peso / (altura*altura)).toFixed(2);
+console.log(soma);
---
+++ atividade03.js
@@ -0,0 +1,25 @@
+//1
+let num1 = 15;
+let num2 = 15;
+console.log(num1 + num2 )
+console.log(num1 - num2 )
+console.log(num1 * num2 )
+console.log(num1 / num2 )
+
+//2
+let idade1 = 500;
+let idade2 = 3000;
+
+let verificar = idade1 > idade2 ? 'a idade 1 é maior' : 'a idade 1 é menor'
+console.log(verificar)
+
+//3
+let idade = 20;
+let comparar = idade<=18 ? 'menor de idade' : 'maior de idade'
+console.log(comparar)
+
+//4
+let num = 20;
+let descobrir = num % 2!= 0 ? 'impar' : 'par'
+console.log(descobrir)
+
